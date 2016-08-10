1c7f374ccaf013c00f3197e976f1b815e79d9f81
Olivier Deckers <deckers.olivier@gmail.com>
2016-08-10 08:46:45 +0200

    Added more unit tests, avoided using TestActorRef because it doesn't work with persistentactors

--- src/test/scala/VideoAggregateSpec.scala
+++ src/test/scala/VideoAggregateSpec.scala
@@ -1,16 +1,45 @@
-import akka.actor.{ActorSystem, Props}
-import akka.testkit.{TestActorRef, TestKit}
-import org.scalatest.{FlatSpec, FlatSpecLike}
+import java.util.UUID
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
+import org.scalatest._
 
 /**
   * Created by olivierdeckers on 08/08/16.
   */
-class VideoAggregateSpec extends TestKit(ActorSystem("spec")) with FlatSpecLike {
+class VideoAggregateSpec extends TestKit(ActorSystem("spec")) with fixture.FlatSpecLike with BeforeAndAfterAll with ImplicitSender with DefaultTimeout {
 
-  "A VideoAggregate" should "create a video" in {
-    val videoAggregate = TestActorRef[VideoAggregate](Props(new VideoAggregate("1")))
-    videoAggregate ! AddVideo("1", "test")
+  "A VideoAggregate" should "create a video" in { tuple =>
+    val videoAggregate = tuple._2
+    videoAggregate ! AddVideo(tuple._1, "test")
+    videoAggregate ! GetVideo(tuple._1)
+    expectMsg(Some(Video(tuple._1, "test")))
+  }
 
-    awaitAssert(videoAggregate.underlyingActor.state === Some(Video("1", "test")))
+  it should "return none if video doesn't exist" in { tuple =>
+    tuple._2 ! GetVideo(tuple._1)
+    expectMsg(None)
+  }
+
+  it should "Recreate state from events when restarted" in { tuple =>
+    val id = tuple._1
+    var videoAggregate = tuple._2
+    videoAggregate ! AddVideo(id, "test")
+    videoAggregate = system.actorOf(Props(new VideoAggregate(id)))
+    videoAggregate ! GetVideo(id)
+    expectMsg(Some(Video(id, "test")))
+  }
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    val id = UUID.randomUUID().toString
+    println(id)
+    val videoAggregate = system.actorOf(Props(new VideoAggregate(id)))
+    test((id, videoAggregate))
+  }
+
+  override type FixtureParam = Tuple2[String, ActorRef]
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
   }
 }
